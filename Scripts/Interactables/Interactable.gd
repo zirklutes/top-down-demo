a2e16185e16b485be296c132daa3d6d382cb5e41
Paul T <hackerman@spaghetticoding.de>
2023-07-02 18:19:23 +0200

    Implemented basic interaction system

---
+++ Scripts/Interactables/Interactable.gd
@@ -0,0 +1,9 @@
+extends Area2D
+# This class can be considered as a "motherclass" for all the future
+# Interactables. It'll mainly just pass signals down to future
+# classes like NPCs, signs, or whatever we want to interact with
+class_name Interactable # In order to be able to reference to this class i need to give it a name
+
+# This function needs to be overwritten by any child class
+func interact():
+	print("I'm a prototype interactable")
--- Scripts/Player/PlayerController.gd
+++ Scripts/Player/PlayerController.gd
@@ -4,6 +4,13 @@ extends CharacterBody2D
 
 var input_vec = Vector2(0,0) # Used to map the input by the user to the actual movement direction
 
+var current_interactable = null # Used to keep track of which interactable we would interact with once we press the interact button
+
+
+func _ready():
+	$InteractionRange.connect("area_entered", on_area_entered)
+	$InteractionRange.connect("area_exited", on_area_exited)
+
 func _physics_process(delta):
 	if Input.is_action_pressed("up"):
 		input_vec.y -= 1
@@ -17,3 +24,16 @@ func _physics_process(delta):
 	velocity = input_vec.normalized() * speed # Normalizing the input vector to make diagonal movement the same speed as cardinal movement
 	move_and_slide()
 	input_vec = Vector2.ZERO
+
+func on_area_entered(area):
+	if area is Interactable:
+		current_interactable = area
+
+func on_area_exited(area):
+	if area is Interactable:
+		current_interactable = null
+
+func _input(event):
+	if event.is_action_released("interact"):
+		if current_interactable:
+			current_interactable.interact()

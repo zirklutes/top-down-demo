28674d2c4b3cdb6d6004168edae96aadb22493c6
Dovile <lenurea@gmail.com>
2023-07-03 22:44:49 +0300

    Add a dialogue addon and simple chat between player and npc

--- Scripts/Interactables/Interactable.gd
+++ Scripts/Interactables/Interactable.gd
@@ -4,6 +4,14 @@ extends Area2D
 # classes like NPCs, signs, or whatever we want to interact with
 class_name Interactable # In order to be able to reference to this class i need to give it a name
 
+const Balloon = preload("res://Dialogues/balloon.tscn")
+
+@export var dialogue_resource: DialogueResource
+@export var dialogue_start: String = "start"
+
+
 # This function needs to be overwritten by any child class
 func interact():
-	print("I'm a prototype interactable")
+	var balloon: Node =  Balloon.instantiate()
+	get_tree().current_scene.add_child(balloon)
+	balloon.start(dialogue_resource, dialogue_start)

cb1caeda41b9e93c15493090e304fb3a206c71c5
Dovile <lenurea@gmail.com>
2023-07-23 00:15:37 +0300

    Add fish to player inventory when interacting with fishing hole

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -2,8 +2,8 @@ extends Node
 
 var current_scene = "Main";
 var target_coordinate = Vector2.ZERO
-var inventory_data: InventoryData = preload("res://PlayerInventory.tres")
-var camp_inventory_data: InventoryData = preload("res://CampInventory.tres")
+var inventory_data: InventoryData = preload("res://Resources/Inventory/PlayerInventory.tres")
+var camp_inventory_data: InventoryData = preload("res://Resources/Inventory/CampInventory.tres")
 
 var inventory_interface
 
--- Scripts/Misc/FishingHole.gd
+++ Scripts/Misc/FishingHole.gd
@@ -1,6 +1,24 @@
 extends Sprite2D
 
+var fish = load("res://Resources/Inventory/Fish.tres")
+
 func add_fish():
-	pass
-	#var player = get_tree().get_nodes_in_group("Player")[0]
-	#player.inventory.items["fish"] = 1
+	var fish_slot_data = InventorySlotData.new()
+	fish_slot_data.inventory_item_data = fish
+	fish_slot_data.quantity = 1
+	
+	var available_slot_index
+	var slots = Global.inventory_data.inventory_slots_data
+	
+	for index in slots.size():
+		var item = slots[index]
+		
+		if !item || (item.inventory_item_data.name == fish.name && item.quantity < 20):
+			available_slot_index = index
+			break
+			
+	#Q. Smarter way to check if exists including 0?
+	if available_slot_index || available_slot_index == 0:
+		Global.inventory_data.drop_single_slot_data(fish_slot_data, available_slot_index)
+	else:
+		print("Show warning inventory full")
